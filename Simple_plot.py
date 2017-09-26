1c65c8c9c3ddec5f5c331080858a85b6a4457d72
Riyaz Ul Haque <30811072+RiyazUlHaque@users.noreply.github.com>
2017-09-26 06:56:01 -0700

    Add files via upload

---
+++ Simple_plot.py
@@ -0,0 +1,7 @@
+#plot a simple graph consisting x & y label.
+
+import matplotlib.pyplot as plt  #import matplotlib.pyplot
+plt.plot([1,2,3,4])
+plt.ylabel('Y-AXIS')
+plt.xlabel('X-AXIX')
+plt.show()
